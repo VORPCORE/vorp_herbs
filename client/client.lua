fcdba78104a73a507271128ead713e53cd9d8160
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-01-04 03:45:36 +0000

    fix(client): callback spam calling + delete plants on resource stop for debug

--- client/client.lua
+++ client/client.lua
@@ -14,44 +14,46 @@ local function CreatePickPrompt(promptText, controlAction)
     UiPromptSetEnabled(Prompt, false)
     UiPromptSetVisible(Prompt, false)
     UiPromptSetHoldMode(Prompt, 1000)
-    UiPromptSetGroup(Prompt, Group)
+    UiPromptSetGroup(Prompt, Group, 0)
     UiPromptRegisterEnd(Prompt)
 end
 
 local function PlayerPick(destination, index, plantCoords, isProp)
     UiPromptSetEnabled(Prompt, false)
     UiPromptSetVisible(Prompt, false)
-    isPicking = true
-    VorpCore.Callback.TriggerAsync("vorp_herbs:CheckItemsCapacity", function(canCarryAll, looted)
-        if canCarryAll then
-            local ped = PlayerPedId()
-            TaskTurnPedToFaceCoord(ped, plantCoords, -1)
-            Wait(2000)
-            ClearPedTasks(ped)
-            local dict = "mech_ransack@shelf@h150cm@d80cm@reach_up@pickup@vertical@right_50cm@a"
-            RequestAnimDict(dict)
-            while not HasAnimDictLoaded(dict) do
-                Wait(0)
+    if not isPicking then
+        isPicking = true
+        VorpCore.Callback.TriggerAsync("vorp_herbs:CheckItemsCapacity", function(canCarryAll, looted)
+            if canCarryAll then
+                local ped = PlayerPedId()
+                TaskTurnPedToFaceCoord(ped, plantCoords.x, plantCoords.y, plantCoords.z, -1)
+                Wait(2000)
+                ClearPedTasks(ped)
+                local dict = "mech_ransack@shelf@h150cm@d80cm@reach_up@pickup@vertical@right_50cm@a"
+                RequestAnimDict(dict)
+                while not HasAnimDictLoaded(dict) do
+                    Wait(0)
+                end
+                TaskPlayAnim(ped, dict, "enter_rf", 8.0, 8.0, -1, 1, 0, false, false, false)
+                TaskPlayAnim(ped, dict, "base", 8.0, 8.0, -1, 1, 0, false, false, false)
+                RemoveAnimDict(dict)
+                Wait(700)
+                ClearPedTasks(ped)
+            elseif not canCarryAll and looted then
+                VorpCore.NotifyRightTip(Config.Language.cantpick, 4000)
+            elseif not canCarryAll then
+                VorpCore.NotifyRightTip(Config.Language.NoRoomForItems, 4000)
             end
-            TaskPlayAnim(ped, dict, "enter_rf", 8.0, 8.0, -1, 1, 0, false, false, false)
-            TaskPlayAnim(ped, dict, "base", 8.0, 8.0, -1, 1, 0, false, false, false)
-            RemoveAnimDict(dict)
-            Wait(700)
-            ClearPedTasks(ped)
-        elseif not canCarryAll and looted then
-            VorpCore.NotifyRightTip(Config.Language.cantpick, 4000)
-        elseif not canCarryAll then
-            VorpCore.NotifyRightTip(Config.Language.NoRoomForItems, 4000)
-        end
-        isPicking = false
-
-    end, destination, index, plantCoords, isProp)
+            Wait(1000)
+            isPicking = false
+        end, destination, index, plantCoords, isProp)
+    end
 end
 
 local function CreatePlant(destination, index)
     local plantModel = GetHashKey(destination.plantModel)
     if not HasModelLoaded(plantModel) then
-        RequestModel(plantModel)
+        RequestModel(plantModel, false)
         repeat Wait(0) until HasModelLoaded(plantModel)
     end
     local plantModelObject = CreateObject(plantModel, destination.coords.x, destination.coords.y, destination.coords.z,
@@ -79,12 +81,13 @@ function GetClosestObject(coords, prophash)
     end
     return closestObject
 end
+
 CreateThread(function()
     repeat Wait(5000) until LocalPlayer.state.IsInSession
     CreatePickPrompt(Config.Language.PromptText, Config.ControlAction)
-     local function getDist(pedcoords,coords)
-       if not coords then return  end
-       return #(pedcoords - coords)
+    local function getDist(pedcoords, coords)
+        if not coords then return end
+        return #(pedcoords - coords)
     end
     while true do
         local sleep = 1000
@@ -92,11 +95,11 @@ CreateThread(function()
         local pedCoords = GetEntityCoords(ped)
         if not IsEntityDead(ped) then
             for k, v in pairs(Config.Plants) do
-                local distance = getDist(pedCoords,v.coords)
+                local distance = getDist(pedCoords, v.coords)
                 if v.placeprop then
-                    if distance and  distance <= 100 then
-                        if not v.plant then 
-                            CreatePlant(v, k, pedCoords)
+                    if distance and distance <= 100 then
+                        if not v.plant then
+                            CreatePlant(v, k)
                         end
                     else
                         if v.plant then
@@ -111,13 +114,13 @@ CreateThread(function()
                     pedCoords = GetEntityCoords(ped)
                     GroupName = Config.Language.PromptGroupName .. ": " .. v.name
                     GroupName = VarString(10, "LITERAL_STRING", GroupName)
-                    UiPromptSetActiveGroupThisFrame(Group, GroupName)
+                    UiPromptSetActiveGroupThisFrame(Group, GroupName, 0, 0, 0, 0)
                     UiPromptSetEnabled(Prompt, true)
                     UiPromptSetVisible(Prompt, true)
 
                     if UiPromptHasHoldModeCompleted(Prompt) then
                         isPicking = true
-                        PlayerPick(v, k, v.coords, false)     -- location
+                        PlayerPick(v, k, v.coords, false) -- location
                     end
                 end
             end
@@ -137,37 +140,47 @@ CreateThread(function()
         local pedCoords = GetEntityCoords(ped)
         if not IsEntityDead(ped) then
             for k, v in pairs(Config.Plants) do
-               if not v.placeprop and not v.islocation then 
-                local plantModel = GetHashKey(v.plantModel)
-                local plantDetected = DoesObjectOfTypeExistAtCoords(pedCoords.x, pedCoords.y, pedCoords.z,
-                    Config.MinimumDistance, plantModel, false)
-                if not isPicking and plantDetected == 1  then
-                    if not v.plantModel or v.plantModel == "" then
-                        print("must add the plant model for the look up in config")
-                    end
-                    sleep = 0
-                    GroupName = Config.Language.PromptGroupName .. ": " .. v.name
-                    GroupName = VarString(10, "LITERAL_STRING", GroupName)
-                    UiPromptSetActiveGroupThisFrame(Group, GroupName)
-                    UiPromptSetEnabled(Prompt, true)
-                    UiPromptSetVisible(Prompt, true)
-
-                    if UiPromptHasHoldModeCompleted(Prompt) then
-                        local plantEntity = GetClosestObject(pedCoords, plantModel)
-                        if plantEntity then
-                            local plantCoords = GetEntityCoords(plantEntity)
-                            PlayerPick(v, k, plantCoords, true) -- prop
-                        else
-                            print("No plant entity found nearby")
+                if not v.placeprop and not v.islocation then
+                    local plantModel = GetHashKey(v.plantModel)
+                    local plantDetected = DoesObjectOfTypeExistAtCoords(pedCoords.x, pedCoords.y, pedCoords.z,
+                        Config.MinimumDistance, plantModel, false)
+                    if not isPicking and plantDetected == 1 then
+                        if not v.plantModel or v.plantModel == "" then
+                            print("must add the plant model for the look up in config")
                         end
-                    end
-                
+                        sleep = 0
+                        GroupName = Config.Language.PromptGroupName .. ": " .. v.name
+                        GroupName = VarString(10, "LITERAL_STRING", GroupName)
+                        UiPromptSetActiveGroupThisFrame(Group, GroupName, 0, 0, 0, 0)
+                        UiPromptSetEnabled(Prompt, true)
+                        UiPromptSetVisible(Prompt, true)
 
-                    break
-                end
+                        if UiPromptHasHoldModeCompleted(Prompt) then
+                            local plantEntity = GetClosestObject(pedCoords, plantModel)
+                            if plantEntity then
+                                local plantCoords = GetEntityCoords(plantEntity)
+                                PlayerPick(v, k, plantCoords, true) -- prop
+                            else
+                                print("No plant entity found nearby")
+                            end
+                        end
+
+                        break
+                    end
                 end
             end
         end
         Wait(sleep)
+    end
+end)
+
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if resourceName == 'vorp_herbs' then
+        for _, v in pairs(Config.Plants) do
+            if v.plant then
+                DeleteEntity(v.plant)
+            end
+        end
     end
 end)
--- server/server.lua
+++ server/server.lua
@@ -2,59 +2,60 @@ local VorpCore = exports.vorp_core:GetCore()
 local CoordsLooted = {}
 local PropsLooted = {}
 
-VorpCore.Callback.Register("vorp_herbs:CheckItemsCapacity", function(source, callback, destination, key, plantCoords, isProp)
-    local _source = source
-    local itemsToGive = {}
+VorpCore.Callback.Register("vorp_herbs:CheckItemsCapacity",
+    function(source, callback, destination, key, plantCoords, isProp)
+        local _source = source
+        local itemsToGive = {}
 
 
-    if not key then
-        return callback(false)
-    end
+        if not key then
+            return callback(false)
+        end
 
-    if isProp then
-        for k, v in ipairs(PropsLooted) do
-            if v.x == plantCoords.x and v.y == plantCoords.y and v.z == plantCoords.z then 
-                return callback(false,true)
+        if isProp then
+            for _, v in ipairs(PropsLooted) do
+                if v.x == plantCoords.x and v.y == plantCoords.y and v.z == plantCoords.z then
+                    return callback(false, true)
+                end
+            end
+        else
+            if CoordsLooted[key] then
+                return callback(false, true)
             end
         end
-    else
-        if CoordsLooted[key] then
-            return callback(false, true)
-        end
-    end
 
-    local rewardAmount = math.random(destination.minReward, destination.maxReward)
-    for _, rewardItem in ipairs(destination.reward) do
-        local canCarryItem = exports.vorp_inventory:canCarryItem(_source, rewardItem, rewardAmount)
-        if canCarryItem then
-            table.insert(itemsToGive, { name = rewardItem, count = rewardAmount })
+        local rewardAmount = math.random(destination.minReward, destination.maxReward)
+        for _, rewardItem in ipairs(destination.reward) do
+            local canCarryItem = exports.vorp_inventory:canCarryItem(_source, rewardItem, rewardAmount)
+            if canCarryItem then
+                table.insert(itemsToGive, { name = rewardItem, count = rewardAmount })
+            end
         end
-    end
 
-    if #itemsToGive > 0 then
-        for _, item in ipairs(itemsToGive) do
-            exports.vorp_inventory:addItem(_source, item.name, item.count)
-            VorpCore.NotifyRightTip(_source, "You got " .. item.count .. "x " .. item.name, 4000)
-        end
-        table.wipe(itemsToGive)
-        if isProp then
-            table.insert(PropsLooted, plantCoords)
-            SetTimeout(destination.cooldown * 60000, function()
-                for i, coords in ipairs(PropsLooted) do
-                    if coords.x == plantCoords.x and coords.y == plantCoords.y and coords.z == plantCoords.z then
-                        table.remove(PropsLooted, i)
-                        break
+        if #itemsToGive > 0 then
+            for _, item in ipairs(itemsToGive) do
+                exports.vorp_inventory:addItem(_source, item.name, item.count)
+                VorpCore.NotifyRightTip(_source, "You got " .. item.count .. "x " .. item.name, 4000)
+            end
+            table.wipe(itemsToGive)
+            if isProp then
+                table.insert(PropsLooted, plantCoords)
+                SetTimeout(destination.cooldown * 60000, function()
+                    for i, coords in ipairs(PropsLooted) do
+                        if coords.x == plantCoords.x and coords.y == plantCoords.y and coords.z == plantCoords.z then
+                            table.remove(PropsLooted, i)
+                            break
+                        end
                     end
-                end
-            end)
-        else
-            CoordsLooted[key] = true
-            SetTimeout(destination.cooldown * 60000, function()
-                CoordsLooted[key] = nil
-            end)
+                end)
+            else
+                CoordsLooted[key] = true
+                SetTimeout(destination.cooldown * 60000, function()
+                    CoordsLooted[key] = nil
+                end)
+            end
+            return callback(true)
         end
-        return callback(true)
-    end
 
-    return callback(false)
-end)
+        return callback(false)
+    end)
